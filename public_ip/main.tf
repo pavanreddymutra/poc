8c9a2489dca64ea601807f878e1e0a92b4834af3
pavanreddymutra <88705522+pavanreddymutra@users.noreply.github.com>
2023-06-19 12:01:21 +0530

    chaging sku

--- public_ip/main.tf
+++ public_ip/main.tf
@@ -3,5 +3,6 @@ resource "azurerm_public_ip" "public_ip" {
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
   tags =var.tags
 }
